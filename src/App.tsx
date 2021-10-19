7d27e365d85fc6b3f314358163aa94a009f2e1cf
raphael luiz schettino <raphaelluiz128@hotmail.com>
2021-10-19 11:07:11 -0300

    modify movent and imgems from char

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
+import { useEffect } from 'react';
 import * as C from './App.styles';
 import { Char } from './components/char';
+import { useChar } from './hooks/useChar';
 
 const App = () => {
+  const char = useChar('Raphael');
+  const char2 = useChar('Boniecky');
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+        char.moveLeft();
+      break;
+      case 'KeyW':
+      case 'ArrowUp':
+        char.moveUp();
+      break;
+      case 'KeyD':
+      case 'ArrowRight':
+        char.moveRight();
+      break;
+      case 'KeyS':
+      case 'ArrowDown':
+        char.moveDown();
+      break;
+    }
+  }
+
   return (
-  <C.Container>
-    <C.Map>
-      <Char />
-    </C.Map>
-  </C.Container>
+    <C.Container>
+      <C.Map>
+        <Char x={char.x} y={char.y} side={char.side} name={char.name} />
+        <Char x={char2.x} y={char2.y} side={char2.side} name={char2.name} />
+      </C.Map>
+    </C.Container>
   );
 }
 
--- src/components/char/index.tsx
+++ src/components/char/index.tsx
@@ -1,8 +1,30 @@
 import * as C from './styles';
+import { CharacterSides } from '../../types/CharacterSides';
 
-export const Char = () => {
+type Props = {
+    x: number;
+    y: number;
+    side: CharacterSides;
+    name: string;
+}
+
+export const Char = ({x, y, side, name}: Props) => {
+    const size = 30;
+    const sides = {
+        down: 0,
+        left: -30,
+        right: -60,
+        up: -90
+    }
+
     return (
-        <C.Container>
+        <C.Container
+            size={size}
+            left={x * size}
+            top={y * size}
+            sidePos={sides[side] ?? 0}
+        >
+            <C.NameBox>{name}</C.NameBox>
         </C.Container>
     );
-};
+}
